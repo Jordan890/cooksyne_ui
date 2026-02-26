c183ac41949ced6cad5bac06d1f9a8d28437f4f1
Jordan Ahlers <jahlers46@gmail.com>
2026-02-26 16:30:36 -0700

    Hide links if user is not logged in

--- cart-and-cook-ui/src/app/features/home/pages/home-page/home-page.ts
+++ cart-and-cook-ui/src/app/features/home/pages/home-page/home-page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { WelcomeCard } from '../../components/welcome-card/welcome-card';
 import { QuickLinksCard } from '../../components/quick-links-card/quick-links-card';
+import { AuthService } from '../../../../core/auth/auth';
 
 @Component({
   selector: 'app-home-page',
@@ -10,5 +11,7 @@ import { QuickLinksCard } from '../../components/quick-links-card/quick-links-ca
   styleUrl: './home-page.scss',
 })
 export class HomePage {
+
+  constructor(public auth: AuthService) {}
 
 }
